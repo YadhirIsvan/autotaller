13d244e5ebc347ac32e612124bb65339d9881837
ronaldo <ronaldo@kaliii.kaliii.com>
2025-11-27 10:56:40 -0600

    autotaller commit3

--- autotaller/urls.py
+++ autotaller/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# autotaller/autotaller/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    # Incluye las urls de cada app
+    path("", include("core.urls")),             # home / endpoints del core
+    path("servicios/", include("servicios.urls")),
+    path("solicitudes/", include("solicitudes.urls")),
 ]
